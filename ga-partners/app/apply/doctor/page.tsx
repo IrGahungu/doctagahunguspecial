d6d59ec8e95dd94285b99c95bc71ffa926c0b6a9
IrGahungu <gahungujean12@gmail.com>
2026-06-18 19:46:51 +0530

    Updates

--- ga-partners/app/apply/doctor/page.tsx
+++ ga-partners/app/apply/doctor/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import DoctorPageContent from "./DoctorPageContent";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
